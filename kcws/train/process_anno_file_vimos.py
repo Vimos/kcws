5565966da08ad98a74b8dc4fa6a374a367db2432
Vimos Tan <vimostan@gmail.com>
2016-12-08 15:02:49 +0800

    rename parameters

--- kcws/train/process_anno_file_vimos.py
+++ kcws/train/process_anno_file_vimos.py
@@ -10,20 +10,20 @@ pat1 = re.compile(ur"( ?\[.+?\].+? +|.+? +)")
 pat2 = re.compile(ur"\[(?P<first_name>.+?)\]")
 
 
-def processItem(line):
+def process_item(line):
     for item in pat1.findall(line):
         item = item.strip()
         if item.startswith('['):
-            for iitem in pat2.findall(item):
-                for zitem in iitem.split(' '):
-                    yield zitem
+            for i_item in pat2.findall(item):
+                for z_item in i_item.split(' '):
+                    yield z_item
         else:
             yield item
 
 
-def processLine(line):
+def process_line(line):
     s = []
-    for item in processItem(line):
+    for item in process_item(line):
         try:
             token, _ = item.rsplit("/", 1)
             s.extend(list(token))
@@ -38,10 +38,10 @@ def processLine(line):
     yield sentence
 
 
-def processFile(curFile):
-    with codecs.open(curFile, "r", encoding="utf8") as fp:
+def process_file(cur_file):
+    with codecs.open(cur_file, "r", encoding="utf8") as fp:
         for l in fp:
-            for s in processLine(l):
+            for s in process_line(l):
                 if s:
                     yield s
 
@@ -50,11 +50,11 @@ def main(argc, argv):
     if argc < 3:
         print("Usage:%s <dir> <output>" % (argv[0]))
         sys.exit(1)
-    rootDir = argv[1]
+    root_dir = argv[1]
     with codecs.open(argv[2], "w", encoding='utf8') as out:
-        for dirName, _, fileList in os.walk(rootDir):
-            for f in filter(lambda x: x.endswith(".txt"), fileList):
-                [out.write(u"{}\n".format(s)) for s in processFile(os.path.join(rootDir, dirName, f))]
+        for dir_name, _, file_list in os.walk(root_dir):
+            for f in filter(lambda x: x.endswith(".txt"), file_list):
+                [out.write(u"{}\n".format(s)) for s in process_file(os.path.join(root_dir, dir_name, f))]
 
 
 if __name__ == '__main__':
